87cfe7d93060fa3667bb2ba352cd4e8e5d98cbd5
David Speck <david@saduras.de>
2020-08-19 17:48:58 +0200

    Add Logging

--- Engine/src/Engine/EntryPoint.h
+++ Engine/src/Engine/EntryPoint.h
@@ -7,6 +7,12 @@ extern Engine::Application* Engine::CreateApplication();
 
 int main(int argc, char** argv)
 {
+	Engine::Log::Init();
+	ENG_CORE_FATAL("Testing Fatal");
+	ENG_CORE_WARN("Initialized Log!");
+	int a = 5;
+	ENG_CLIENT_INFO("Hello Var={0}", a);
+
 	auto app = Engine::CreateApplication();
 	app->Run();
 	delete app;
