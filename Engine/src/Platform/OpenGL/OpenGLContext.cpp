091cc46a02441b05c2d3eaf316f4c39347b00bcd
David Speck <david@saduras.de>
2020-09-04 16:53:34 +0200

    Render first triangle

--- Engine/src/Platform/OpenGL/OpenGLContext.cpp
+++ Engine/src/Platform/OpenGL/OpenGLContext.cpp
@@ -17,6 +17,12 @@ namespace Engine
 		glfwMakeContextCurrent(m_WindowHandle);
 		int status = gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
 		ENG_CORE_ASSERT(status, "Failed to initialize Glad!");
+
+		ENG_CORE_INFO("OpenGL Info:");
+		ENG_CORE_INFO("  Vendor: {0}", glGetString(GL_VENDOR));
+		ENG_CORE_INFO("  Renderer: {0}", glGetString(GL_RENDERER));
+		ENG_CORE_INFO("  Version: {0}", glGetString(GL_VERSION));
+
 	}
 
 	void OpenGLContext::SwapBuffers()
