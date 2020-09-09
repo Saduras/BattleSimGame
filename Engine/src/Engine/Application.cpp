9b7e0187951c251c6cbb91a4aeef94d6a45707d2
David Speck <david@saduras.de>
2020-09-09 13:41:03 +0200

    Introduce BufferLayout

--- Engine/src/Engine/Application.cpp
+++ Engine/src/Engine/Application.cpp
@@ -11,6 +11,28 @@ namespace Engine
 {
 	Application* Application::s_Instance = nullptr;
 
+	static GLenum ShaderDataTypeToOpenGLBaseType(ShaderDataType type)
+	{
+		switch (type) {
+			case Engine::ShaderDataType::Float:
+			case Engine::ShaderDataType::Float2:
+			case Engine::ShaderDataType::Float3:
+			case Engine::ShaderDataType::Float4:
+			case Engine::ShaderDataType::Mat3:
+			case Engine::ShaderDataType::Mat4:
+				return GL_FLOAT;
+			case Engine::ShaderDataType::Int:
+			case Engine::ShaderDataType::Int2:
+			case Engine::ShaderDataType::Int3:
+			case Engine::ShaderDataType::Int4:
+				return GL_INT;
+			case Engine::ShaderDataType::Bool:
+				return GL_BOOL;
+		}
+		ENG_CORE_ASSERT(false, "Unknown ShaderDataType!");
+		return 0;
+	}
+
 	Application::Application()
 	{
 		ENG_CORE_ASSERT(!s_Instance, "Application already exists.");
@@ -19,16 +41,35 @@ namespace Engine
 		m_Window = std::unique_ptr<Window>(Window::Create());
 		m_Window->SetEventCallback(ENG_BIND_EVENT_FN(Application::OnEvent));
 
-		float verticies[3 * 3] = {
-			-0.5f, -0.5f, 0.0f,
-			 0.5f, -0.5f, 0.0f,
-			 0.0f,  0.5f, 0.0f,
+		float verticies[3 * 7] = {
+			-0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 1.0f, 1.0f,
+			 0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 1.0f,
+			 0.0f,  0.5f, 0.0f, 1.0f, 1.0f, 0.0f, 1.0f,
 		};
 
 		m_VertexBuffer.reset(VertexBuffer::Create(verticies, sizeof(verticies)));
 
-		glEnableVertexAttribArray(0);
-		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), nullptr);
+		{
+			BufferLayout layout = {
+				{ ShaderDataType::Float3, "a_Position" },
+				{ ShaderDataType::Float4, "a_Color" }
+			};
+
+			m_VertexBuffer->SetLayout(layout);
+		}
+
+		uint32_t index = 0;
+		const auto& layout = m_VertexBuffer->GetLayout();
+		for (const auto& element : layout) {
+			glEnableVertexAttribArray(index);
+			glVertexAttribPointer(index, 
+				element.GetElementCount(), 
+				ShaderDataTypeToOpenGLBaseType(element.Type),
+				element.Normalized ? GL_TRUE : GL_FALSE,
+				layout.GetStride(),
+				(const void*)(size_t)element.Offset);
+			index++;
+		}
 
 		unsigned int indicies[3] = { 0, 1, 2 };
 		m_IndexBuffer.reset(IndexBuffer::Create(indicies, sizeof(indicies) / sizeof(uint32_t)));
@@ -37,12 +78,15 @@ namespace Engine
 			#version 330 core
 			
 			layout(location = 0) in vec3 a_Position;
+			layout(location = 1) in vec4 a_Color;
 
 			out vec3 v_Position; 
+			out vec4 v_Color;
 
 			void main()
 			{
 				v_Position = a_Position;
+				v_Color = a_Color;
 				gl_Position = vec4(a_Position, 1.0);
 			}
 		)";
@@ -52,10 +96,11 @@ namespace Engine
 			layout(location = 0) out vec4 color;
 
 			in vec3 v_Position;
+			in vec4 v_Color;
 
 			void main()
 			{
-				color = vec4(v_Position * 0.5 + 0.5, 1.0);
+				color = v_Color;
 			}
 		)";
 
@@ -92,7 +137,7 @@ namespace Engine
 	void Application::Run()
 	{
 		while (m_Running) {
-			glClearColor(0.1, 0.1, 0.1, 1);
+			glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
 			glClear(GL_COLOR_BUFFER_BIT);
 
 			m_Shader->Bind();
