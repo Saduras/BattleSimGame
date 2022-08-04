e1c506048e84a370122f5c80700cfa6b39da759b
David Speck <david@saduras.de>
2022-08-04 15:14:59 +0200

    Add util to compare vector<int> for unit tests

--- EngineUnitTests/src/AssertUtils.h
+++ EngineUnitTests/src/AssertUtils.h
@@ -23,3 +23,24 @@ namespace AssertUtils {
 		return std::wstring(str.begin(), str.end());
 	}
 };
+
+namespace Microsoft
+{
+	namespace VisualStudio
+	{
+		namespace CppUnitTestFramework
+		{
+			template<> static std::wstring ToString<std::vector<int>>(const class std::vector<int>& vec)
+			{
+				std::wstringstream ss;
+				for (size_t i = 0; i < vec.size(); i++)
+				{
+					if (i != 0)
+						ss << ", ";
+					ss << vec[i];
+				}
+				return ss.str();
+			}
+		}
+	}
+}
