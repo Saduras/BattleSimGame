325d11dc1e15054dcd71dfc148f20670de4fa5b9
David Speck <david@saduras.de>
2022-08-27 11:02:22 +0200

    Disable VSync for better performance metrics

--- Engine/src/Platform/Windows/WindowsWindow.cpp
+++ Engine/src/Platform/Windows/WindowsWindow.cpp
@@ -56,7 +56,7 @@ namespace Engine
 		m_Context->Init();
 
 		glfwSetWindowUserPointer(m_Window, &m_Data);
-		SetVSync(true);
+		SetVSync(false);
 
 		// set GLFW callbacks
 		glfwSetWindowSizeCallback(m_Window, [](GLFWwindow* window, int width, int height) {
